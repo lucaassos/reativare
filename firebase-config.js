300b8fd647454df0b7fba36243902855527fa865
lucaassos <152328656+lucaassos@users.noreply.github.com>
2025-09-03 13:51:00 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDiWDy54Gphn2kopi07yGGdbxgKK62bC4s",
+  authDomain: "reativare-109ec.firebaseapp.com",
+  projectId: "reativare-109ec",
+  storageBucket: "reativare-109ec.firebasestorage.app",
+  messagingSenderId: "667339560747",
+  appId: "1:667339560747:web:2d36c156df5f5eb2b7576e"
+};
