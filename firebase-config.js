80edda14c4c796823e1353a7ec3055994bff0955
lucaassos <152328656+lucaassos@users.noreply.github.com>
2025-09-03 13:51:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
+// ATENÇÃO: COLE AQUI A CONFIGURAÇÃO DO SEU PROJETO FIREBASE
+// Você encontra isso no Console do Firebase > Configurações do Projeto > Geral > Seus apps
 const firebaseConfig = {
-  apiKey: "AIzaSyDiWDy54Gphn2kopi07yGGdbxgKK62bC4s",
-  authDomain: "reativare-109ec.firebaseapp.com",
-  projectId: "reativare-109ec",
-  storageBucket: "reativare-109ec.firebasestorage.app",
-  messagingSenderId: "667339560747",
-  appId: "1:667339560747:web:2d36c156df5f5eb2b7576e"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SUA_APP_ID"
 };
+
+// Inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Disponibilizar os serviços do Firebase para os outros scripts
+const auth = firebase.auth();
+const db = firebase.firestore();
